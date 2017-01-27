f2a0a326ded0f5bb3ec8b780efcda8abebbf111c
marrtan <kawin919@gmail.com>
2017-01-27 13:03:15 +0700

    Edit Route

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function() {
+
+	Route::get('/', function () {
+	    return view('welcome');
+	});
+
+	Route::get('movie', 'MovieController@index');
+	Route::get('upload-movie', 'MovieController@uploadMovie');
+	Route::get('update-movie', 'MovieController@update');
+	Route::get('delete-movie/{id}', 'MovieController@delete');
+
+	Route::get('movie/{id}', 'AudioController@index');
+	Route::get('audios', 'AudioController@getAudios');
+	Route::get('upload-audio', 'AudioController@uploadAudio');
+	Route::get('update-audio', 'AudioController@update');
+	Route::get('delete-audio', 'AudioController@delete');
+
+	Route::get('recorder', 'UploadController@index');
+	Route::post('upload-record', array('as' => 'upload-record', 
+	    'uses' => 'UploadController@uploadRecord'));
+
+	Route::get('player/{id}', 'PlayerController@index');
+	Route::get('player', 'PlayerController@playerList');
+
 });
-
-Route::get('movie', 'MovieController@index');
-Route::get('upload-movie', 'MovieController@uploadMovie');
-Route::get('update-movie', 'MovieController@update');
-Route::get('delete-movie/{id}', 'MovieController@delete');
-
-Route::get('movie/{id}', 'AudioController@index');
-Route::get('audios', 'AudioController@getAudios');
-Route::get('upload-audio', 'AudioController@uploadAudio');
-Route::get('update-audio', 'AudioController@update');
-Route::get('delete-audio', 'AudioController@delete');
-
-Route::get('recorder', 'UploadController@index');
-Route::post('upload-record', array('as' => 'upload-record', 
-    'uses' => 'UploadController@uploadRecord'));
-
-Route::get('player/{id}', 'PlayerController@index');
-Route::get('player', 'PlayerController@playerList');
